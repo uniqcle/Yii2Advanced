a99d65700c56f55455ebe5410b73fb9ecb51591b
uniqcle <uniqcle@yandex.ru>
2019-06-29 13:00:22 +0000

    Включаем модуль отладчика frontend/config/main-local.php

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -14,7 +14,7 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-
+        'allowedIPs' => ['*', '::1'],  # Включаем все IP
     ];
 
     $config['bootstrap'][] = 'gii';
