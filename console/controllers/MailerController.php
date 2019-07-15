2c502c89d7d3eb438f8c412972c50e1f7d181417
Andrey Anuchkin <uniqcle@yandex.ru>
2019-07-15 20:59:35 +0700

    Простое консольное приложение

---
+++ console/controllers/MailerController.php
@@ -0,0 +1,11 @@
+<?php 
+namespace console\controllers; 
+
+use yii\console\Controller; 
+
+class MailerController extends Controller
+{
+	public function actionSend(){
+		echo 'Hello, world!'; 
+	}
+}
